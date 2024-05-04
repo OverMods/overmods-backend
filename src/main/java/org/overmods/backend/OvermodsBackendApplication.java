665fcc62d29fe0739b28383bbfac9298c953c7b0
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-05-04 08:09:08 +0300

    spring and gradle initial setup

---
+++ src/main/java/org/overmods/backend/OvermodsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.overmods.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvermodsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvermodsBackendApplication.class, args);
+    }
+
+}
