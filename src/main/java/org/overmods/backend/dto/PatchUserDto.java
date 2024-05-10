0a7302cc7ae4add6737ac3f58f320703bd6d7dd2
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-05-10 17:34:43 +0300

    make patch user dto ignore unknown fields, cuz, we got little time to tinker with allat bs

--- src/main/java/org/overmods/backend/dto/PatchUserDto.java
+++ src/main/java/org/overmods/backend/dto/PatchUserDto.java
@@ -1,10 +1,12 @@
 package org.overmods.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.Optional;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PatchUserDto {
     public Optional<@NotBlank @Email String> email;
 
