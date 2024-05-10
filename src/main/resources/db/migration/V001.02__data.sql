4b4d3ebb40a3e6c117641d1c9ffe078ca84184d5
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-05-10 14:59:21 +0300

    do not directly insert ids into tables, because that would mess postgres sequences and later inserts will fail

--- src/main/resources/db/migration/V001.02__data.sql
+++ src/main/resources/db/migration/V001.02__data.sql
@@ -1,7 +1,7 @@
-INSERT INTO "overmods"."user" (id, username, email, password, color,
+INSERT INTO "overmods"."user" (username, email, password, color,
                                role, site_rating, registered_at, updated_at,
                                password_changed, banned, deleted)
 VALUES
-    (1, 'admin', 'admin@overmods.org', '$2a$10$3E/ANd9vZMvcji07znpPw.EKHNITKN9T.bUWq4GqrO6uu9oXD1EDq', NULL,
+    ('admin', 'admin@overmods.org', '$2a$10$3E/ANd9vZMvcji07znpPw.EKHNITKN9T.bUWq4GqrO6uu9oXD1EDq', NULL,
      'ADMIN', NULL, '2024-05-09 16:09:49.695315', '2024-05-09 16:09:49.695315', '2024-05-09 16:09:49.695315',
      FALSE, FALSE);
